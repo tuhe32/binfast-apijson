e9d191e500632566fefab2c5fd190a810a0fe08f
liubin <tuhe32@163.com>
2022-12-02 09:47:05 +0800

    first commit

---
+++ src/main/java/com/binfast/apijson/BinfastApijsonApplication.java
@@ -0,0 +1,13 @@
+package com.binfast.apijson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinfastApijsonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BinfastApijsonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/binfast/apijson/BinfastApijsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binfast.apijson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinfastApijsonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
