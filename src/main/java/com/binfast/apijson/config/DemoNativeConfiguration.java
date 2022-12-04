b63c561f0143cf4a1f50ef9c7498a097086ce8c0
liubin <tuhe32@163.com>
2022-12-04 16:35:38 +0800

    fix: 增加apijson

--- src/main/java/com/binfast/apijson/config/DemoNativeConfiguration.java
+++ src/main/java/com/binfast/apijson/config/DemoNativeConfiguration.java
@@ -1,9 +1,7 @@
 package com.binfast.apijson.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.nativex.hint.AotProxyHint;
+import org.springframework.nativex.hint.JdkProxyHint;
 import org.springframework.nativex.hint.NativeHint;
-import org.springframework.nativex.hint.TypeHint;
 import org.springframework.nativex.type.NativeConfiguration;
 
 import javax.servlet.http.HttpServletRequest;
@@ -13,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
  * @author 刘斌
  * @date 2022/12/4 3:06 下午
  */
-@NativeHint(aotProxies = @AotProxyHint(interfaces = {
+@NativeHint(jdkProxies = @JdkProxyHint(types = {
         HttpServletRequest.class,
         HttpServletResponse.class}
 )
